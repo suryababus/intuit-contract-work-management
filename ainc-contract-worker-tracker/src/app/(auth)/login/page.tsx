600c0e2479865d89bf21e2c94f12d858d3498ad8
Your Name <you@example.com>
2024-08-14 10:43:22 +0530

    login state fix

--- ainc-contract-worker-tracker/src/app/(auth)/login/page.tsx
+++ ainc-contract-worker-tracker/src/app/(auth)/login/page.tsx
@@ -1,6 +1,29 @@
+"use client";
+
 import { LoginForm } from "@/components/block/login-form";
+import { FullPageLoader } from "@/components/ui/full-page-loader";
+import { useAuth } from "@/state/auth";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { init, loading, isAuthenticated } = useAuth();
+  useEffect(() => {
+    init();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen w-screen">
+        <FullPageLoader />
+      </div>
+    );
+  }
+
+  if (isAuthenticated) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex-1 flex items-center justify-center">
       <LoginForm />
